028e21b34bc3f15614bca9aca2a65899a70d59b2
S00310830-IsaacLavergne <s00310830@atu.ie>
2026-09-17 16:56:04 +0100

    This is a worksheet 2 commit test

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,37 @@
 import express, {Application, Request, Response} from "express" ;
+import carRoutes from './routes/cars';
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
 
+app.use(express.json());
+app.use('/api/v1/cars', carRoutes);
+app.use((req, _res, next) => {  
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
-    message: "hello from Una"
+    message: "hello from Una chafdsafdsaf"
+    });
+});
+
+app.get('/bananas', async (_req : Request, res: Response) => {
+    res.json({
+    message: "this is bananas",
+    });
+});
+
+app.get('/goober', async (_req : Request, res: Response) => {
+    res.json({
+    message: "this is the goober page, welcome goober",
     });
 });
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
     });
+
+    
